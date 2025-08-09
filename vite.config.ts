578d855fc5b145d613b0f22b3277956d03379a0f
lg047 <88402180+lg047@users.noreply.github.com>
2025-08-09 02:23:17 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import { defineConfig } from "vite";
 // Set base to "/<REPO_NAME>/" for GitHub Pages.
 // Replace REPO_NAME before deploy.
 export default defineConfig({
-  base: "/roo-lounge/",
+  base: "/yuyu-lounge/",
 });

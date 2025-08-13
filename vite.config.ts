42f91d30c3bbb956f73254ff372af94bb39d444e
lg047 <88402180+lg047@users.noreply.github.com>
2025-08-13 16:39:31 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   // Ensure correct asset paths on GitHub Pages
-  base: "/yuyu-lounge/",
+  base: "/",
   build: {
     outDir: "dist",
     emptyOutDir: true
